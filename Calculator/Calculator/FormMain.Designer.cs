f74314dea5849a2bfb5f1a42a2edb390ef681408
zerazaN <zerazaN@gmail.com>
2020-12-11 14:17:39 +0300

    Start
    Ready + button
    DoubleTextContainer

---
+++ Calculator/Calculator/FormMain.Designer.cs
@@ -0,0 +1,89 @@
+﻿namespace Calculator
+{
+    partial class FormMain
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.WorkNum = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // WorkNum
+            // 
+            this.WorkNum.CausesValidation = false;
+            this.WorkNum.Location = new System.Drawing.Point(283, 46);
+            this.WorkNum.Name = "WorkNum";
+            this.WorkNum.Size = new System.Drawing.Size(100, 20);
+            this.WorkNum.TabIndex = 0;
+            this.WorkNum.Text = "0";
+            this.WorkNum.TextChanged += new System.EventHandler(this.WorkNum_TextChanged);
+            this.WorkNum.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.WorkNum_KeyPress);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(347, 461);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "label1";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(296, 127);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "+";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // FormMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(715, 799);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.WorkNum);
+            this.Name = "FormMain";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.FormMain_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox WorkNum;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ Calculator/Calculator/FormMain.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Calculator
+{
+    public partial class FormMain : Form
+    {
+        private double workNumber = 0;
+        private double Number = 0;
+        public FormMain()
+        {
+            InitializeComponent();
+        }
+
+        private void FormMain_Load(object sender, EventArgs e)
+        {
+            WorkNum.Select();
+            label1.Text = Number.ToString();
+        }
+
+        private void WorkNum_KeyPress(object sender, System.Windows.Forms.KeyPressEventArgs e)
+        {
+            if (!char.IsControl(e.KeyChar) && !char.IsDigit(e.KeyChar) &&
+                (e.KeyChar != '.') && (e.KeyChar != '-'))
+            {
+                e.Handled = true;
+            }
+
+            if ((e.KeyChar == '.') && (((sender as TextBox).Text.IndexOf('.') > -1) || 
+                (sender as TextBox).Text.Trim() == "" || (sender as TextBox).Text.Trim() == "-"))
+            {
+                e.Handled = true;
+            }
+
+            if ((e.KeyChar == '-') && ((sender as TextBox).Text.Trim() != ""))
+            {
+                e.Handled = true;
+            }
+
+        }
+
+        private void WorkNum_TextChanged(object sender, EventArgs e)
+        {
+            workNumber = Convert.ToDouble(WorkNum.Text);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Number += workNumber;
+            label1.Text = Number.ToString();
+        }
+    }
+}
